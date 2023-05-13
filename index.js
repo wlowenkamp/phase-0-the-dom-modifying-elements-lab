d811d5dc69c91cc42d6e4ea8e7c7e378cdca0bc6
Will Lowenkamp <lowenkamp97@gmail.com>
2023-05-13 12:15:19 -0400

    changes for assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let newMain = document.getElementById("main");
+newMain.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Will is the champion";
+document.body.append(newHeader);
